8599aa48d40e4a7df8cb60bc36fdec260deee379
Vlada Oleynik <voleynik3221@gmail.com>
2015-06-16 16:59:39 +0300

    DB projecting

--- notes/models.py
+++ notes/models.py
@@ -6,13 +6,22 @@ from django.contrib.auth.models import User
 class Color(models.Model):
     color = models.CharField(max_length=6)
 
+    def __unicode__(self):
+        return self.color
+
 
 class Tag(models.Model):
     tag = models.CharField(max_length=40)
 
+    def __unicode__(self):
+        return self.tag
+
 
 class Category(models.Model):
     category = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.category
 
 
 class UserSetting(models.Model):
@@ -21,12 +30,19 @@ class UserSetting(models.Model):
     id_tag = models.ManyToManyField(Tag, blank=True)
     id_category = models.ManyToManyField(Category, blank=True)
 
+    def __unicode__(self):
+        return self.user__username
+
 
 class Note(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=40)
     text = models.TextField()
     media = models.FileField(blank=True)
+    permission = models.BooleanField(default=True)
     id_color = models.OneToOneField(Color)
     id_tag = models.ManyToManyField(Tag, blank=True)
     id_category = models.ManyToManyField(Category)
+
+    def __unicode__(self):
+        return '{0} - {1}'.format(self.user__username, self.title)
