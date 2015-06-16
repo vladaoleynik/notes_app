9e48e43243a8482363821b1436ea4152d5319d89
Vlada Oleynik <voleynik3221@gmail.com>
2015-06-16 16:20:30 +0300

    DB projecting

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from notes import models
 
 # Register your models here.
+admin.site.register(models.UserSetting)
+admin.site.register(models.Tag)
+admin.site.register(models.Note)
+admin.site.register(models.Color)
+admin.site.register(models.Category)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Color(models.Model):
+    color = models.CharField(max_length=6)
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=40)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=40)
+
+
+class UserSetting(models.Model):
+    user = models.OneToOneField(User)
+    id_color = models.ManyToManyField(Color, blank=True)
+    id_tag = models.ManyToManyField(Tag, blank=True)
+    id_category = models.ManyToManyField(Category, blank=True)
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=40)
+    text = models.TextField()
+    media = models.FileField(blank=True)
+    id_color = models.OneToOneField(Color)
+    id_tag = models.ManyToManyField(Tag, blank=True)
+    id_category = models.ManyToManyField(Category)
