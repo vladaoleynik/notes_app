ac30f987df4497628e716ad1342ea3d1eefab854
Vlada Oleynik <voleynik3221@gmail.com>
2015-06-20 13:57:49 +0300

    Adding url reverse

--- notes/views.py
+++ notes/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'notes/index.html'
+
+    def get(self, request, *args, **kwargs):
+        notes = dict()
+        return render(request, self.template_name, notes)
