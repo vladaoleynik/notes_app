d0102a13886b57f25cc315c581da2c2d23a4f1d9
Vlada Oleynik <voleynik3221@gmail.com>
2015-06-20 21:43:42 +0300

    Pagination

--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import actions
 
 
@@ -9,6 +10,27 @@ class IndexView(TemplateView):
 
     def get(self, request, *args, **kwargs):
         notes = actions.get_notes_list()
+        paginator = Paginator(notes, 4)  # Show 4 notes per page
+
+        page = request.GET.get('page')
+        try:
+            notes = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            notes = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            notes = paginator.page(paginator.num_pages)
+
+        # count = actions.get_notes_count(self.request.user)
+        return render(request, self.template_name, {'notes': notes})
+
+
+class MyNotesView(TemplateView):
+    template_name = 'notes/index.html'
+
+    def get(self, request, *args, **kwargs):
+        notes = actions.get_author_notes(str(request.user))
         return render(request, self.template_name, {'notes': notes})
 
 
@@ -16,21 +38,27 @@ class NotesAuthorView(TemplateView):
     template_name = 'notes/index.html'
 
     def get(self, request, *args, **kwargs):
+        author = kwargs['username']
         notes = actions.get_author_notes(kwargs['username'])
-        return render(request, self.template_name, {'notes': notes})
+        info = "Author notes: " + author
+        return render(request, self.template_name, {'notes': notes, 'info': info})
 
 
 class NotesCategoryView(TemplateView):
     template_name = 'notes/index.html'
 
     def get(self, request, *args, **kwargs):
-        notes = actions.get_category_notes(kwargs['category'])
-        return render(request, self.template_name, {'notes': notes})
+        category = kwargs['category']
+        notes = actions.get_category_notes(category)
+        info = "Notes by category: " + category
+        return render(request, self.template_name, {'notes': notes, 'info': info})
 
 
 class NotesTagView(TemplateView):
     template_name = 'notes/index.html'
 
     def get(self, request, *args, **kwargs):
+        tag = kwargs['tag']
         notes = actions.get_tag_notes(kwargs['tag'])
-        return render(request, self.template_name, {'notes': notes})
+        info = "Notes by tag: " + tag
+        return render(request, self.template_name, {'notes': notes, 'info': info})
