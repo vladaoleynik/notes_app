dd353b8621b7a28558579fcc0d4817864af7b617
Vlada Oleynik <voleynik3221@gmail.com>
2015-06-22 03:50:12 +0300

    Add note

--- core/settings.py
+++ core/settings.py
@@ -115,6 +115,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = 'var/www/booksnob/media/static/'
+MEDIA_ROOT = 'var/www/booksnob/media/'
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+)
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
